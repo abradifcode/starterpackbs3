53c8264d56984f8e4c9db3b57e572662f66b6e90
buriato <bair.ungo@gmail.com>
2017-09-04 03:31:13 +0200

    Update img.js
    enable copying of all types of images

--- gulp/tasks/img.js
+++ gulp/tasks/img.js
@@ -1,11 +1,11 @@
 module.exports = function () {
   $.gulp.task('img:dev', () => {
-    return $.gulp.src('./dev/static/img/**/*.{png,jpg,gif}')
+    return $.gulp.src('./dev/static/img/**/*.*')
       .pipe($.gulp.dest('./build/static/img/'));
   });
 
   $.gulp.task('img:build', () => {
-    return $.gulp.src('./dev/static/img/**/*.{png,jpg,gif}')
+    return $.gulp.src('./dev/static/img/**/*.*')
       .pipe($.gp.imagemin())
       .pipe($.gulp.dest('./build/static/img/'));
   });
